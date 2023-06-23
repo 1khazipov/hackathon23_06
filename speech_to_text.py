2ec3abd66d35c563be2f42d36acd42fdd4e2fe31
Albert Khazipov <aaooo00aa0@gmail.com>
2023-06-23 22:44:32 +0300

    cresate speech_to_text.py

--- speech_to_text.py
+++ speech_to_text.py
@@ -1,6 +1,14 @@
 import torch
+import speech_recognition as sr
 
 def text_with_punctuation(text):
     model, example_texts, languages, punct, apply_te = torch.hub.load(repo_or_dir='snakers4/silero-models',
                                                                       model='silero_te')
     return apply_te(text, lan='ru')
+
+def speech_to_text(audio) -> str:
+    r = sr.Recognizer()
+    with sr.AudioFile(audio) as source:
+        audio_data = r.record(source)
+        text = r.recognize_google(audio_data)
+    return text_with_punctuation(text)
