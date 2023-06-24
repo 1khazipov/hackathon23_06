29d565c9440a8b9e6046f226b0c59144b6218395
Dima <15Dnau@gmail.com>
2023-06-24 18:46:45 +0300

    For integration

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
-from timecodes import get_timestamped
+from timecodes import get_timestamped, get_sentences
 from screenshot import ScreenshotSaver
+#from speech_to_text import make_paragraphs
 
-generator = get_timestamped('/home/dima/Downloads/ru.wav')
+word_list = get_timestamped('/home/dima/Downloads/small.mp4')
+sentences = get_sentences(word_list)
+
+saver = ScreenshotSaver(video_path="/home/dima/Downloads/small.mp4",
+                    output_path="/home/dima/Desktop/vscode_projects/hackaton/data_example",
+                    screenshots_path='/home/dima/Desktop/vscode_projects/hackaton/screenshots')
+
+out = saver.run_algorithm(sentences=sentences)
+print(out)
 
 
-n = ScreenshotSaver("/home/dima/Downloads/example.mp4", "/home/dima/Desktop/vscode_projects/hackaton/data_example", '/home/dima/Desktop/vscode_projects/hackaton/screenshots')
-n.get_frames()
-errors = n.calculate_similarity('/home/dima/Desktop/vscode_projects/hackaton/data_example')
-n.save_screenshots(error_list=errors)
