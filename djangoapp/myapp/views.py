012ea11e9e99cfeac181f061c4b8fac112440bd6
Artur Yakupov <art.kazan@mail.ru>
2023-06-25 04:18:29 +0300

    some changes

--- djangoapp/myapp/views.py
+++ djangoapp/myapp/views.py
@@ -42,11 +42,10 @@ async def download_side_effect(request):
             kwargs = get_args(request.POST)
             print(kwargs)
             # todo: контент рут вынести в settings, запускать скачивание внутри pipelineservice
-            info = {'id': video_id, 'link': link}
             print(link, video_id)
-            run(link, video_id, kwargs)
-            #threading.Thread(target=run, args=()).start()
-            context = {"details": info}
+            #run(link, video_id, kwargs)
+            threading.Thread(target=run, args=(link, video_id, kwargs)).start()
+            context = {'id': video_id, 'link': link}
 
         except ResourceUnavailableException:
             context = {"error": "Видео не доступно."}
